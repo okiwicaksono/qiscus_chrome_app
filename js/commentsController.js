21c476804ae2a8bc935e304cbc288eeca070d6dc
kokok <sunupf@gmail.com>
2014-09-15 16:21:22 +0700

    post and list comment

--- js/commentsController.js
+++ js/commentsController.js
@@ -1,71 +1,49 @@
 angular.module("qiscusApp",[])
   .controller("commentsController",['$scope','$http',function($scope,$http){
-    var roomId = 123;
-    var topic = 21;
+    var roomId = 2141;
+    var topic = 4284;
     
     var login = {
-      'user[email]':"oki@qiscus.com",
-      'user[password]':"qiscusisgreat"
+      'user[email]':"sunupf@gmail.com",
+      'user[password]':"Cup680sage874%"
     };
     
-  console.log(login);
-   $http({
-     url:'https://www.qisc.us/users/sign_in.json',
-     method:"POST",
-     data:login
-   }).
+    var token = "mUuFUhDXELsfkCKbssZ7";
+
+    
+    $http({
+      url:'https://www.qisc.us/api/v1/mobile/topic/'+topic+'/comment/'+100000+'/token/'+token,
+      method:"GET",
+      data:""
+    }).
     success(function(data) {
-      console.log(data);
+      $scope.comments = data.results.comments;
     });
-    /*var token = getToken(username,password);
+
     
-    var unformatedComments = getComments(roomId,topic,token);
     
-    var $scope.comments = reformatChat(unformatedComments);
+    $scope.postComment = function($event){
+      if($event.keyCode === 13){
+        var message = $scope.message;
+        $http({
+          url:'https://www.qisc.us/api/v1/mobile/postcomment',
+          method:"POST",
+          data:{
+            'token' : token,
+            'comment' : message,
+            'topic_id' : topic
+          }
+
+        }).
+        success(function(data) {
+          $scope.message = "";
+          console.log(data);
+        }).error(function(error){
+          console.log('error');         
+        });
+      }
+    } 
     
-    var sendComment = postComment($scope.comment,topic,token)*/
-    
+        
   }]);
 
-
-
-/**
- * Function untuk mereformat list comment yang didapat dari server kedalam bentuk pertanggal posting
- * @param   {Object} comments koment lists yang didapat dari server
- * @returns {Object} object yang telah diformat ulang bentuknya
- */
-function reformatChat(comments){
-  return data;
-}
-
-/**
- * Function untuk mendapat kan list comment dari room tertentu dan topic tertentu
- * @param   {Number} room  id room yang dilihat
- * @param   {Number} topic id topic yang dipilih
- * @param   {String} token token yang didapat saat login
- * @returns {Object} list comment yang belum terformat per hari
- */
-function getComments(room,topic,token){
-  return data;
-}
-
-/**
- * Function untuk mendapatkan token, token digunakan untuk mengakses room dan topic dalam aplikasi
- * @param   {String} email    email pengguna
- * @param   {String} password password pengguna
- * @returns {String} token, didapat jika berhasil login
- */
-function getToken(email,password){
-  return token;
-}
-
-/**
- * function untuk menge-post comment
- * @param   {String}  comment text yang akan di post
- * @param   {Number}  topic   id dari topic yang active
- * @param   {String}  token   token
- * @returns {Boolean} return status apakah post berhasil atau tidak
- */
-function postComment(comment,topic,token){
-  return status;
-}
